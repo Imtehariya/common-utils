410c7a55e1937e9199ab058638b58463d204a275
Asheesh Tehariya <asheeshtehariya.sharma905@gmail.com>
2025-02-27 10:33:47 +0530

    Updated the latest functions

--- common/auth.py
+++ common/auth.py
@@ -3,17 +3,32 @@ from typing import Optional
 from jose import jwt, JWTError
 from sqlalchemy.orm import Session
 from .models import User  # Import your SQLAlchemy User model
-from .config import SECRET_KEY  # Import your secret key
+from .config import SECRET_KEY, ALGORITHM  # Import your secret key
 
 def create_access_token(user_id: int, expires_in: int = 24):
     expiration = datetime.now() + timedelta(hours=expires_in)
-    token = jwt.encode({"user_id": user_id, "exp": expiration}, SECRET_KEY, algorithm="HS256")
+    token = jwt.encode({"user_id": user_id, "exp": expiration}, SECRET_KEY, algorithm=ALGORITHM)
     return token
 
+def verify_forgot_access_token(token: str):
+    try:
+        # Decode the JWT token
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])        
+        email: str = payload.get("sub")
+        
+        if email is None:
+            return None  # Invalid token
+
+        return email
+    except jwt.ExpiredSignatureError:
+        return None  # Token expired
+    except JWTError:
+        return None  # Invalid token
+    
 def verify_access_token(token: str, db: Session) -> Optional[User]:
     try:
         # Decode the JWT token
-        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         user_id: int = payload.get("user_id")
         
         if user_id is None:
